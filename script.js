8e2013814eb503d5d16d19c76bfb9fed662e89e2
lavisse <chloe.lavisse80@gmail.com>
2018-10-08 10:54:18 +0200

    envoi des fichiers

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = prompt('Entrez votre prénom:');
+// Vérifications :
+/* if (isNaN(firstName)){
+    alert('Bonjour, ' + firstName);
+   }else{
+    alert('Erreur !');
+  }; */
+  alert('Bonjour, ' + firstName);
